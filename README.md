6b09307b18b626940b3956633b4cdfa44651503f
unknown <nandiirawan327@gmail.com>
2023-12-18 21:50:23 +0700

    feature: create first commit and push to spk-uas

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tugas UAS SPK
+
+Nama : Nandi Irawan <br>
+NIM : 201011400551 <br>
+Kelas : 07TPLP012 <br>
+
+## Install requirements
+
+`pip install -r requirements.txt`
+
+## Run the app
+
+Untuk menjalankan:
+`python main.py`
---
+++ nandi_SPK_Model/nandi.SQl.sql
@@ -0,0 +1,11 @@
+INSERT INTO smartphonepoco (brand,ram,prosesor,storage,baterai,harga,ukuran_layar) VALUES
+	 ('POCO M3','2 GB','MediaTek Helio G35','32 GB','5.000 mAh','Rp.1.499.000','6.5 inch'),
+	 ('POCO X3','4 GB','MediaTek Helio G35','64 GB','6.000 mAh','Rp.1.999.000','6.5 inch'),
+	 ('POCO F3','8 GB','Qualcomm Snapdragon 720G','128 GB','4.500 mAh','Rp.3.999.000','6.4 inch'),
+	 ('POCO X4 Pro','8 GB','MediaTek Dimensity 1000+','128 GB','4.500 mAh','Rp.4.999.000','6.55 inch'),
+	 ('POCO X5','8 GB','MediaTek Dimensity 1200','128 GB','4.500 mAh','Rp.4.999.000','6.43 inch'),
+	 ('POCO X6','12 GB','Qualcomm Snapdragon 865','128 GB','4.200 mAh','Rp.7.999.000','6.44 inch'),
+	 ('POCO M4','4 GB','MediaTek Helio G85','64 GB','6.000 mAh','Rp.2.499.000','6.5 inch'),
+	 ('POCO F4','8 GB','MediaTek Dimensity 810','128 GB','5.000 mAh','Rp.3.999.000','6.5 inch'),
+	 ('POCO X7 Pro','12 GB','Qualcomm Snapdragon 870','128 GB','4.500 mAh','Rp.5.999.000','6.55 inch'),
+	 ('POCO X8 2','12 GB','Qualcomm Snapdragon 898','128 GB','5.000 mAh','Rp.5.970.000','6.62 inch');
